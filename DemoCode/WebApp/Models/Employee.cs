cf55d2b18890ae9ba303ff52245ab9a300ab4018
Sourish Chakraborty <52107607+sochaty@users.noreply.github.com>
2023-01-24 21:48:06 +0800

    added democode (#36)
    * added democode

---
+++ DemoCode/WebApp/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApp.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Fullname { get; set; }
+        public string Department { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+    }
+}
