5484a95186a67cfccb45478797bc36a8b1f9ba7b
Sourish Chakraborty <sourish1978@gmail.com>
2023-02-08 13:42:28 +0800

    updated config

--- scripts/sonarqube.sh
+++ scripts/sonarqube.sh
@@ -79,7 +79,7 @@ sudo chmod 777 ~/../../etc/nginx/sites-available/sonarqube
 cat <<EOT> ~/../../etc/nginx/sites-available/sonarqube
 server{
     listen      80;
-    server_name sonarqube.groophy.in;
+    # server_name sonarqube.groophy.in;
     access_log  /var/log/nginx/sonar.access.log;
     error_log   /var/log/nginx/sonar.error.log;
     proxy_buffers 16 64k;
@@ -89,9 +89,9 @@ server{
         proxy_next_upstream error timeout invalid_header http_500 http_502 http_503 http_504;
         proxy_redirect off;
               
-        proxy_set_header    Host            \$host;
-        proxy_set_header    X-Real-IP       \$remote_addr;
-        proxy_set_header    X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header    Host            $host;
+        proxy_set_header    X-Real-IP       $remote_addr;
+        proxy_set_header    X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_set_header    X-Forwarded-Proto http;
     }
 }
