d89b2892df485cb1928a38c32bb5963b4a1b95ad
Sourish Chakraborty <52107607+sochaty@users.noreply.github.com>
2023-02-15 12:06:10 +0800

    updated postgres and sonarq scripts (#48)
    * nexus image implemented
    
    * implemented postgres server image
    
    * implemented sonarqube server image
    
    * implemented shell scripts for configuring servers
    
    * initial workflow for creating jenkins server image
    
    * test the workflow
    
    * modified action event and step name
    
    * added packer setup,init,validate steps
    
    * fixed path of packer file
    
    * changed packer file path to directory
    
    * fixed shell script path
    
    * segregated steps into jobs
    
    * added branch condition
    
    * moved packer actions in same job
    
    * added sudo apt-get autoremove
    
    * implemented path filter in workflow
    
    * added sudo apt-get upgrade
    
    * added new nexus workflow
    
    * implemented full nexus workflow
    
    * added jenkins,nexus badges
    
    * implemented composite action
    
    * fixed paths filter in events
    
    * fixed evet trigger to pull_request
    
    * testing triggers
    
    * fixed action sequence and some event path filter
    
    * fixed action path
    
    * fixed composite action path
    
    * fix composite action path
    
    * fix composite action_1
    
    * changed default value
    
    * reverted back from composite actions
    
    * implemented postgres workflow
    
    * edit for triggering workflow
    
    * removed duplicate packer file
    
    * script path modified
    
    * chnaged workflow trigger to manual
    
    * added manual workflow trigger
    
    * implemented sonarqube workflow
    
    * modified job name
    
    * added democode
    
    * added terraform  code
    
    * added Jenkinsfile
    
    * fixed restore packages stage
    
    * fixed stage Restore packages step parameter
    
    * added serilog nuget reference
    
    * updated readme for badges
    
    * updated jenkins script
    
    * updated badges
    
    * implemented server infra
    
    * removed few unwanted files
    
    * updated gitignore
    
    * added installation of java
    
    * added new configuration in script,fixed sonar port
    
    * added new configuration
    
    * fixed port for sonarqube
    
    * resolved conflict
    
    * updated configuration
    
    * updated config
    
    * updated postgres and sonarq scripts

--- scripts/postgres.sh
+++ scripts/postgres.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 sudo cp ../../etc/sysctl.conf ../../root/sysctl.conf_backup
-sudo chmod 777 ../../etc/sysctl.conf
-cat <<EOT>> ../../etc/sysctl.conf
+sudo chmod 777 ~/../../etc/sysctl.conf
+
+cat <<EOT>> ~/../../etc/sysctl.conf
 vm.max_map_count=262144
 fs.file-max=65536
 ulimit -n 65536
 ulimit -u 4096
 EOT
-sudo cp ../../etc/security/limits.conf ../../root/sec_limit.conf_backup
-sudo chmod 777 ../../etc/security/limits.conf
-cat <<EOT>> ../../etc/security/limits.conf
+
+sudo cp /etc/security/limits.conf /root/sec_limit.conf_backup
+sudo chmod 777 ~/../../etc/security/limits.conf
+
+cat <<EOT> ~/../../etc/security/limits.conf
 sonarqube   -   nofile   65536
 sonarqube   -   nproc    409
 EOT
+
 sudo apt-get update -y
 sudo apt-get install openjdk-11-jdk -y
 sudo update-alternatives --config java
 java -version
+
 sudo apt-get update -y
 sudo wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
@@ -31,6 +36,4 @@ sudo -i -u postgres psql -c "ALTER USER sonar WITH ENCRYPTED PASSWORD 'sonar.adm
 sudo -i -u postgres psql -c "CREATE DATABASE sonarqube OWNER sonar;"
 sudo -i -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE sonarqube to sonar;"
 sudo systemctl restart  postgresql
-
-#systemctl status -l   postgresql
-# sudo netstat -tulpena | grep postgres
+sudo reboot
